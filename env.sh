38ba6af7f70ab23b90260752ab58eb90859bf78e
LTancock <l.j.tancock@gmail.com>
2022-03-08 10:18:42 +0000

    env

--- env.sh
+++ env.sh
@@ -1,7 +1,6 @@
 # Add any `module load` or `export` commands that your code needs to
 # compile and run to this file.
-
-module load languages/gcc/9.3.0
-
+module load GCC/7.2.0-2.29
+module load slurm-21.08
 export OMP_NUM_THREADS=28
 export OMP_PLACES=cores
